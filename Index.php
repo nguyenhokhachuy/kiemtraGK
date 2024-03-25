07b4906f40644eaf2dbadca1abca96d8b5114f8b
KhacHuy <huy12a6ap@gmail.com>
2024-03-25 12:59:55 +0700

    first commit

---
+++ Index.php
@@ -0,0 +1,10 @@
+<?php include_once ('header.php'); ?>
+<ul>
+    <li>
+        <a href="/KTGK/list_nhanvien.php">Danh sách nhân viên</a>
+    </li>
+    <li>
+        <a href="/KTGK/add_nhanvien.php">Thêm nhân viên</a>
+    </li>
+</ul>
+<?php include_once ('footer.php'); ?>
---
+++ footer.php
@@ -0,0 +1,7 @@
+</div>
+<footer>
+    &#169; 2024 - Nguyễn Hồ Khắc Huy
+</footer>
+</body>
+
+</html>
